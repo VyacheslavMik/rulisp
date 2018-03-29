5e5b8c711f52ae2b54c2b87d5cc6230a53060843
Hedin <vyacheslav.mik.8@gmail.com>
2018-03-29 09:47:15 +0300

    Fix file headers

--- src/forum/forum.lisp
+++ src/forum/forum.lisp
@@ -1,6 +1,6 @@
 ;;;; forum.lisp
 ;;;;
-;;;; This file is part of the restas-forum library, released under Lisp-LGPL.
+;;;; This file is part of the rulisp application, released under GNU Affero General Public License, Version 3.0
 ;;;; See file COPYING for details.
 ;;;;
 ;;;; Author: Moskvitin Andrey <archimag@gmail.com>
